6cbd6b9e1e444cecdbd06fb45fc048663b69bd48
MarcP <marcpm1994@gmail.com>
2020-12-13 21:14:30 +0100

    Lecture 3 exercise subsample

--- src/utils.jl
+++ src/utils.jl
@@ -24,3 +24,24 @@ function binaryBinSearch( array::Array{T1,1}, query::T2 ) where {T1<:Number,T2<:
     
     return lo
 end
+
+function downsample( image, step )
+    h, w = size(image)
+    rowindices = 1:step:h
+    colindices = 1:step:w
+    
+    subsampled_img = zeros( eltype(image), length(rowindices), length(colindices) )
+    
+    ccont = 1
+    for col in colindices
+        rcont = 1
+        for row in rowindices
+            
+            subsampled_img[rcont,ccont] = image[ row, col ]
+            rcont += 1
+        end
+        ccont += 1
+    end
+    
+    return subsampled_img
+end
